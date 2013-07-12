5e4300d52d1adb9961453c699ec8256650620540
calzoneman <calzoneman@gmail.com>
2013-07-12 16:58:02 -0400

    Start working on module-based channel

---
+++ cmodules.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "playlist": {
+        server: "cmodules/playlist.js",
+        client: ""
+    }
+}
---
+++ priority.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HIGH: 0,
+    MEDIUM: 10,
+    LOW: 20,
+    MONITOR: 100
+}
